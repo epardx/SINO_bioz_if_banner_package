cc83c996e44643f042f48c9085ebe83140b8dc4e
Ehud Pardo <udi@my-machine.local>
2025-01-06 05:21:11 -0800

    progress on if banner sino package

--- src/config_vars.js
+++ src/config_vars.js
@@ -2,11 +2,14 @@ function  config_vars() {
 	return {
 		company: _vendor_id,
 		vendor: _vendor_name,
+		main_id: _vendor_id + "-ci",
 		size: "100",
 		vendor_partner_color: _vendor_partner_color,
 		be_form_url: "https://back-badge-8.bioz.com",
 		home_banner_background_image: "1",
 		no_iframe_render: "1",
+		ex_title: "0",
+		version: "1",
 		start_time: new Date(),
 		key: '1bG20yPy070QH2428M09'
 	};
--- src/index.js
+++ src/index.js
@@ -11,5 +11,7 @@ import './keyboard.js';
 
 import {bioz_w_attach_message} from "./bioz-w-api.min.js";
 import {v_widget_noiframe_obj} from "./v_widget.sino.js";
+import {ci_widget_noiframe_obj} from "./v_widget.sino.js";
 bioz_w_attach_message();
 export {v_widget_noiframe_obj};
+export {ci_widget_noiframe_obj};
